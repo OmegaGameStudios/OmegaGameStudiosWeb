0b048700c1d1efc61e0451d1d1e0c7db2fe5a572
marcusonline97 <Mjpersson97@gmail.com>
2023-12-22 20:40:43 +0100

    Applied Mockup

--- OmegaGameStudios/OmegaGameStudios_App/OmegaGameStudios/Pages/EmployeeOverview.razor.cs
+++ OmegaGameStudios/OmegaGameStudios_App/OmegaGameStudios/Pages/EmployeeOverview.razor.cs
@@ -1,13 +1,16 @@
-﻿using OmegaGameStudios.Shared.Domain;
+﻿using Microsoft.AspNetCore.Components;
+using OmegaGameStudios.Shared.Domain;
 using OmegaGameStudios.Shared.Models;
 namespace OmegaGameStudios_App.Pages
 {
-    public partial class EmployeeOverview
+    public partial class EmployeeOverview : ComponentBase
     {
         public List<Employee> Employees { get; set; } = default!;
  
-
-
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
     }
 
 }
